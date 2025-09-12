c2360736f20327ee6bdc33e61e6b5df1e2f89300
Dungeonking644 <CER240098@spengergasse.at>
2025-09-12 10:21:03 +0200

    hineinspeichern

---
+++ Brüche.js
@@ -0,0 +1,46 @@
+const b1 = "3 3/4";
+const b2 = "7";
+const b3 = "3/4";
+const b4 = "5 6/5";
+
+
+function string_to_bruch (s)
+{
+    let bruch = {};
+    
+    let leerzeichensplit = s.split (" ");
+
+    if(leerzeichensplit.length == 2 )
+    {
+        
+       let slashsplit =  leerzeichensplit[1].split ("/");
+
+        bruch.ganzeZahl = Number(leerzeichensplit[0]);
+        bruch.zaehler = Number(slashsplit[0]);
+        bruch.nenner = Number(slashsplit[1]);
+    }
+
+    else if(leerzeichensplit.length == 1)
+    {
+        let bruchsplit = leerzeichensplit[0].split("/");
+
+        if(bruchsplit.length == 2 )
+        {
+        bruch.zaehler = Number(bruchsplit[0]);
+        bruch.nenner = Number(bruchsplit[1]); 
+        }
+        else
+        {
+            bruch.ganzeZahl = Number(s);
+        }
+    }
+    
+    console.log(bruch);
+    return bruch;
+}
+
+let bruchstring1 = string_to_bruch(b1);
+let bruchstring4 = string_to_bruch(b4)
+string_to_bruch(b2);
+string_to_bruch(b3);
+
