d0799871337fd1e02165df9e843bef4e8695f452
Dungeonking644 <CER240098@spengergasse.at>
2026-03-16 14:01:22 +0100

    ms import

---
+++ 2026-03-16_transpile/src/script.ts
@@ -0,0 +1,26 @@
+import { holeEssen, loescheEssen } from "./essen.ts"; // so ists ideal
+import ms from "ms"; // so ist es auch möglich
+// import * as essensfunctions from "./essen.ts";  -> neues Objekt mit essensfunctions.holeEssen
+// import "./essen.ts"; --> es wird nur das Modul ausgeführt, aber keine Funktionen importiert
+
+type EssenGlobals = typeof globalThis & {
+    holeEssen: typeof holeEssen;
+    loescheEssen: typeof loescheEssen;
+};
+
+const globals = globalThis as EssenGlobals;
+
+globals.holeEssen = holeEssen;
+globals.loescheEssen = loescheEssen;
+
+document.getElementById("hole-essen")?.addEventListener("click", holeEssen);
+document.getElementById("loesche-essen")?.addEventListener(
+    "click",
+    loescheEssen,
+);
+
+document.getElementById("ms-button")?.addEventListener("click", () => {
+    const input = (document.getElementById("ms-input") as HTMLInputElement).value;
+    const output = ms(input);
+    document.getElementById("ms-output")!.textContent = `In Millisekunden: ${output}`;
+});
